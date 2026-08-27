9b2fbba332330cb0062f5118cc19b4e0a6321dc2
fadeykevin <131300463+fadeykevin@users.noreply.github.com>
2026-08-27 01:31:04 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+  const privateBtn = document.getElementById("btn-private");
+  if (privateBtn) {
+    privateBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert("Proyecto privado empresarial sujeto a acuerdo de confidencialidad.");
+    });
+  }
+});
